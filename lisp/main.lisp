12929c50ac92d52e65631d45ffc90223afb5423e
Jason Chandler <jason.chandler@pm.me>
2024-07-03 02:09:28 -0500

    actually working pointers

--- lisp/main.lisp
+++ lisp/main.lisp
@@ -8,7 +8,7 @@
                 "Array arr = Array();
                  arr.resize(1);
                  arr[0] = #0;
-                 LispSingleton::singleton->call_deferred_to_symbol(#1, LispSingleton::singleton, \"get_node\", arr);")))
+                 @(return) = LispSingleton::singleton->call_deferred_to_symbol(#1, LispSingleton::singleton, \"get_node\", arr);")))
 
 (defmacro get-node->symbol (name symbol)
   `(get-node->symbol-impl ,name ',symbol))
@@ -16,34 +16,19 @@
 (defun get-node (name)
   (get-node->symbol-impl name (gensym)))
 
-(get-node->symbol "cam" camera)
-
-(defun get-node (name)
-  (ffi:clines "#include <godot_cpp/classes/object.hpp>")
-  (ffi:clines "#include <godot_cpp/classes/node.hpp>")
-  (ffi:clines "#include <iostream>")
-  (ffi:clines "using namespace godot;")
-  (ffi:c-inline (name) (:cstring) :pointer-void
-                "LispSingleton::singleton->call_deferred(\"get_node\", #0)
-                 Node* node = Object::cast_to<Node>(obj);
-                 std::cout << String(node->get_name()).utf8() << std::endl;"))
-
 (defun get-name (ptr)
   (ffi:clines "#include <godot_cpp/classes/object.hpp>")
   (ffi:clines "#include <godot_cpp/classes/node.hpp>")
   (ffi:clines "using namespace godot;")
-   (ffi:c-inline (ptr) (:pointer-void) :object
+   (ffi:c-inline (ptr) (:pointer-void) :cstring
                 "
                  Node* node = Object::cast_to<Node>(reinterpret_cast<Object*>(#0));
-
                  String str_name = String(node->get_name());
                  CharString utf8_name = str_name.utf8();
                  const char* name = utf8_name.get_data();
-                 char* dynamic_name = new char[(strlen(name))];
-                 strcpy(dynamic_name, name);
-                 std::cout << name << std::endl;
-                 cl_object lisp_string = ecl_make_constant_base_string(dynamic_name, -1);
-                 return lisp_string;"))
+                 @(return)=name;"))
+
+
+(get-node->symbol "cam" camera)
 (get-name camera)
 (princ camera)
-(pointer-to-object camera)
